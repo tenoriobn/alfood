471656cfea16b877b75e697be2b2db89299697ab
Bruno Tenório <tenoriobn@gmail.com>
2023-11-01 11:53:23 -0300

    Formulário de pratos

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/Admi
 import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurante';
 import PaginaBaseAdmin from './paginas/Administracao/Restaurantes/PaginaBaseAdmin';
 import AdministracaoPratos from './paginas/Administracao/Pratos/AdministracaoPratos';
+import FormularioPrato from './paginas/Administracao/Pratos/FormularioPrato';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
 
         <Route path="pratos" element={<AdministracaoPratos />} />
+        <Route path="pratos/novo" element={<FormularioPrato />} />
       </Route>
     </Routes>
   );
